965bb55ebb73264213c5c16ecd169a35ac54f080
olusolabobby <olusolaadelabu89@gmail.com>
2023-03-09 08:32:23 +0200

    single

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,17 +3,22 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
-import React, {useContext, useEffect, useState} from "react";
-import {db} from "../../firebase";
+import React, {useContext, useEffect, useState, useRef} from "react";
+import {db, storage} from "../../firebase";
 import {collection, setDoc, doc, onSnapshot} from "firebase/firestore";
 import {AuthContext} from "../../context/AuthContext";
 import {setFormData} from "../../jsHelpers/js-Helpers";
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 const Single = () => {
 
   const [user, setUser] = useState([]);
+  const [file, setFile] = useState('');
   const [editId, setEditId] = useState('');
   const [editFormValues, setEditFormValues] = useState({});
+  const [perc, setPerc] = useState (null); // to allow the image show in the users collection in firebase
+
 
 
   const userIDToShow = useContext(AuthContext).currentUser.idToShow;
@@ -30,7 +35,7 @@ const Single = () => {
         onSnapshot(collectionRef, (snapshot) => {
           const data = snapshot.docs.map((doc) => ({...doc.data(), id: doc.id}))
           setUser(data?.filter(user => user?.id === userIDToShow))
-          // console.log(list);
+          console.log(data);
         });
       }catch(err){
         console.log(err);
@@ -40,17 +45,61 @@ const Single = () => {
   }, [])
 
   const editUser = async(id, newUser = editFormValues) => {
-    const docRef = doc(db,"users",id);
-    console.log('docRef', docRef);
-    // console.log('editFormValues', newUser);
-    await setDoc(docRef, newUser)
+    const docRef = doc(db,"users", id);
+    // console.log('docRef', docRef);
+    console.log('editFormValues', newUser);
+    await setDoc(docRef, newUser);
   }
 
   const handleSaveEdit = async (id, newUser) => {
-    await editUser(id, newUser);
+    await editUser( id, newUser);
     setEditId('');
     setEditFormValues({});
   }
+
+
+  useEffect ( ()=>{
+
+    const handleUpload = () => {
+      const name = new Date().getTime() + file.name
+      // console.log(name);
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on("state_changed",
+          (snapshot)=> {
+            const progress = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+            console.log(progress, "%");
+            setPerc (progress)
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+              default:
+                break;
+            }
+          },
+          (err)=> console.log(err),
+          ()=>{
+            getDownloadURL(uploadTask.snapshot.ref)
+                // .then(url => console.log(url))
+                .then(url => setEditFormValues(prevState => {
+                  return{
+                    ...prevState,
+                    img: url,
+                    imageName: file.name
+                  }
+                }));
+          }
+      );
+    };
+    file && handleUpload();
+  }, [file]);
+
+
 
   return (
       <div className="single">
@@ -59,9 +108,8 @@ const Single = () => {
           <Navbar />
           <div className="top">
             <div className="left">
-              <div className="editButton" onClick={ ()=> console.log(setEditId(user?.id))}>Edit</div>
+              {/*<div className="editButton" onClick={ ()=> setEditId(user[0].id)}>Edit</div>*/}
               <h1 className="title" >Information</h1>
-
 
               {user && user.map((user) => (
                   editId !== user?.id
@@ -71,7 +119,7 @@ const Single = () => {
                         alt=""
                         className="itemImg"
                     />}
-                    <div className="details">
+                        <div className="details">
                       <h1 className="itemTitle">{user.displayName}</h1>
                       <div className="detailItem">
                         <span className="itemKey">Email:</span>
@@ -83,27 +131,76 @@ const Single = () => {
                       </div>
                       <div className="detailItem">
                         <span className="itemKey">Address:</span>
-                        <span className="itemValue">
-                        {user.address}
-                      </span>
+                        <span className="itemValue">{user.address}</span>
                       </div>
                       <div className="detailItem">
                         <span className="itemKey">Country:</span>
                         <span className="itemValue">{user.country}</span>
                       </div>
+                          <div className="editButton" onClick={ ()=> setEditId(user.id)}>Edit</div>
                     </div>
                   </div>)
                       :
-                      (<div>
-                        <div className="detailItem">
-                          <span className="itemKey">FullName</span>
-                          <input value={ editFormValues?.displayName}
-                          onChange={ (e)=> setFormData(e.target.value, 'displayName',setEditFormValues)}
+                      (<div key={user.id}>
+                        {/*{editFormValues?.img && <img src={editFormValues.img} alt=" " style={{width: "100px"}}/> }*/}
+
+                        <div>
+                          <img style={{width: "100px"}}
+                              src={
+                                file
+                                    ? URL.createObjectURL(file)
+                                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                              }
+                              alt=""
                           />
                         </div>
 
+                        <label htmlFor="file" style={{display: "flex", justifyContent:"flex-start"}}>
+                          Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                        </label>
+                        <input
+                            type="file" id="file"
+                            onChange={(e) => setFile(e.target.files[0])}
+                            style={{ display: "none" }}
+                        />
 
-                        {/*<button type='button' onClick={()=>handleSaveEdit(user.id)}>Save Edit</button>*/}
+                        <div className="detailItem">
+                          <span className="itemKeyEdit">FullName: </span>
+                          <input value={user.displayName}
+                          onChange={ (e)=> setFormData(e.target.value, 'displayName',setEditFormValues)}
+                          />
+                        </div><br/>
+                        {/*<div className="detailItem">*/}
+                        {/*  <span className="itemKeyEdit username">Username: </span>*/}
+                        {/*  <input value={user.username}*/}
+                        {/*         onChange={ (e)=> setFormData(e.target.value, 'username',setEditFormValues)}*/}
+                        {/*  />*/}
+                        {/*</div><br/>*/}
+                        <div className="detailItem">
+                          <span className="itemKeyEdit phone">Phone: </span>
+                          <input value={user.phone}
+                                 onChange={ (e)=> setFormData(e.target.value, 'phone',setEditFormValues)}
+                          />
+                        </div><br/>
+                        {/*<div className="detailItem">*/}
+                        {/*  <span className="itemKeyEdit email">Email: </span>*/}
+                        {/*  <input value={user.email}*/}
+                        {/*         onChange={ (e)=> setFormData(user.email = e.target.value, 'email', setEditFormValues)}*/}
+                        {/*  />*/}
+                        {/*</div><br/>*/}
+                        <div className="detailItem">
+                          <span className="itemKeyEdit address">Address: </span>
+                          <input value={user.address}
+                                 onChange={ (e)=> setFormData(e.target.value, 'address',setEditFormValues)}
+                          />
+                        </div><br/>
+                        <div className="detailItem">
+                          <span className="itemKeyEdit country">Country: </span>
+                          <input value={user.country}
+                                 onChange={ (e)=> setFormData(e.target.value, 'country',setEditFormValues)}
+                          />
+                        </div><br/><br/>
+                        <button disabled={perc !== null && perc < 100 } type="button" onClick={()=>handleSaveEdit(user.id)}>Save</button>
 
                       </div>)
               )) }
